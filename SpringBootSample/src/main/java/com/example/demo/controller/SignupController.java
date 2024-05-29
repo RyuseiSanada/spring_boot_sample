67c32a1dab5f4c17cfdc246257055648e1172e77
RyuseiSanada <sanadaryuusei@gmail.com>
2024-05-30 06:13:05 +0900

    例外処理を実装

--- SpringBootSample/src/main/java/com/example/demo/controller/SignupController.java
+++ SpringBootSample/src/main/java/com/example/demo/controller/SignupController.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,38 +24,56 @@ import com.example.demo.form.SignupForm;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
-@RequestMapping ("/user")
+@RequestMapping("/user")
 @Slf4j
 public class SignupController {
-  
-  @Autowired
-  private UserApplicationService userApplicationService;
-  
-  @Autowired
-  private UserService userService;
-  
-  @Autowired
-  private ModelMapper modelMapper;
-  
-  @GetMapping ("/signup")
-  public String getSignup (Model model, Locale locale, @ModelAttribute SignupForm form) {
-    Map<String, Integer> genderMap = userApplicationService.getGenderMap (locale);
-    model.addAttribute ("genderMap", genderMap);
-    
-    return "user/signup";
-  }
-  
-  @PostMapping ("/signup")
-  public String postSignup (Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
-    
-    if (bindingResult.hasErrors ()) {
-      return getSignup (model, locale, form);
-    }
-    log.info (form.toString ());
-    
-    MUser user = modelMapper.map (form, MUser.class);
-    
-    userService.signup (user);
-    return "redirect:/login";
-  }
+
+	@Autowired
+	private UserApplicationService userApplicationService;
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private ModelMapper modelMapper;
+
+	@GetMapping("/signup")
+	public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
+		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
+		model.addAttribute("genderMap", genderMap);
+
+		return "user/signup";
+	}
+
+	@PostMapping("/signup")
+	public String postSignup(Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+			return getSignup(model, locale, form);
+		}
+		log.info(form.toString());
+
+		MUser user = modelMapper.map(form, MUser.class);
+
+		userService.signup(user);
+		return "redirect:/login";
+	}
+
+	@ExceptionHandler(DataAccessException.class)
+	public String dataAccessExceptionHandler(DataAccessException e, Model model) {
+		model.addAttribute("error", "");
+		model.addAttribute("message", "SignupControllerで例外が発生しました");
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+
+		return "error";
+	}
+
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(Exception e, Model model) {
+		model.addAttribute("error", "");
+		model.addAttribute("message", "SignupControllerで例外が発生しました");
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		return "error";
+	}
+
 }
